d0dd04d2ea6081817f36423e587faae52d0e7e37
andrejatrivic <andreja.trivic25@gmail.com>
2025-06-20 10:42:59 +0200

    #13 Added Index

--- Tutel.EduWork.DataAccessLayer/ApplicationDbContext.cs
+++ Tutel.EduWork.DataAccessLayer/ApplicationDbContext.cs
@@ -13,5 +13,38 @@ namespace Tutel.EduWork.Data
         public DbSet<WorkDay> WorkDays { get; set; }
         public DbSet<WorkSession> WorkSessions { get; set; }
         public DbSet<WorkSessionType> WorkSessionTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var project = builder.Entity<Project>();
+            project.HasIndex(p => p.Name);
+            project.HasIndex(p => p.IsActive);
+            project.HasIndex(p => p.IsBillable);
+
+            var sickLeave = builder.Entity<SickLeave>();
+            sickLeave.HasIndex(s => s.StartDate);
+            sickLeave.HasIndex(s => s.EndDate);
+
+            var user = builder.Entity<ApplicationUser>();
+            user.HasIndex(u => u.Email);
+            user.HasIndex(u => u.Name);
+            user.HasIndex(u => u.Surname);
+
+            var vacation = builder.Entity<Vacation>();
+            vacation.HasIndex(v => v.StartDate);
+            vacation.HasIndex(v => v.EndDate);
+            vacation.HasIndex(v => v.IsTeamBuilding);
+
+            var workDay = builder.Entity<WorkDay>();
+            workDay.HasIndex(w => w.WorkDate);
+            workDay.HasIndex(w => w.WorkDayStart);
+
+            var workSession = builder.Entity<WorkSession>();
+            workSession.HasIndex(w => w.IsOvertime);
+            workSession.HasIndex(w => w.ProjectId);
+            workSession.HasIndex(w => w.TypeId);
+        }
     }
 }
---
+++ Tutel.EduWork.DataAccessLayer/Migrations/20250620084154_AddedIndexing.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tutel.EduWork.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIndexing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkSessions_IsOvertime",
+                table: "WorkSessions",
+                column: "IsOvertime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkDays_WorkDate",
+                table: "WorkDays",
+                column: "WorkDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkDays_WorkDayStart",
+                table: "WorkDays",
+                column: "WorkDayStart");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacations_EndDate",
+                table: "Vacations",
+                column: "EndDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacations_IsTeamBuilding",
+                table: "Vacations",
+                column: "IsTeamBuilding");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacations_StartDate",
+                table: "Vacations",
+                column: "StartDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SickLeaves_EndDate",
+                table: "SickLeaves",
+                column: "EndDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SickLeaves_StartDate",
+                table: "SickLeaves",
+                column: "StartDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_IsActive",
+                table: "Projects",
+                column: "IsActive");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_IsBillable",
+                table: "Projects",
+                column: "IsBillable");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_Name",
+                table: "Projects",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers",
+                column: "Email");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Name",
+                table: "AspNetUsers",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Surname",
+                table: "AspNetUsers",
+                column: "Surname");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_WorkSessions_IsOvertime",
+                table: "WorkSessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkDays_WorkDate",
+                table: "WorkDays");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkDays_WorkDayStart",
+                table: "WorkDays");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vacations_EndDate",
+                table: "Vacations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vacations_IsTeamBuilding",
+                table: "Vacations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vacations_StartDate",
+                table: "Vacations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SickLeaves_EndDate",
+                table: "SickLeaves");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SickLeaves_StartDate",
+                table: "SickLeaves");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_IsActive",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_IsBillable",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_Name",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Surname",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
