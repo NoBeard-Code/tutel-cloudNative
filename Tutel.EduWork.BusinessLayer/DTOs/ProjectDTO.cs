1b1c06ad0725f96277b1b52943f0c9615d983ca6
andrejatrivic <andreja.trivic25@gmail.com>
2025-06-20 13:56:38 +0200

    #14 Changed DTO to public, removed unnecessary imports

--- Tutel.EduWork.BusinessLayer/DTOs/ProjectDTO.cs
+++ Tutel.EduWork.BusinessLayer/DTOs/ProjectDTO.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tutel.EduWork.BusinessLayer.DTOs
+﻿namespace Tutel.EduWork.BusinessLayer.DTOs
 {
-    internal class ProjectDTO
+    public class ProjectDTO
     {
     }
 }
