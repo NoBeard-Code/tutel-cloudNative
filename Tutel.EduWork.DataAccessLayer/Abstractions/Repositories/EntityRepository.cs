fbbe1e21882b97d526ccbc76a5294d27de7b0adb
Marin Grabovac <grabovacmarin000@gmail.com>
2025-06-24 11:29:41 +0200

    #6 Implemented update of the project

---
+++ Tutel.EduWork.DataAccessLayer/Abstractions/Repositories/EntityRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutel.EduWork.Data;
+using Tutel.EduWork.DataAccessLayer.Abstractions.Entities;
+
+namespace Tutel.EduWork.DataAccessLayer.Abstractions.Repositories
+{
+    public abstract class EntityRepository<T> : Repository<T> where T : BaseEntity
+    {
+        protected EntityRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task UpdateAsync(T entity)
+        {
+            var local = Entities.Local.FirstOrDefault(e => e.Id == entity.Id);
+            if (local != null)
+            {
+                Context.Entry(local).State = EntityState.Detached;
+            }
+
+            Entities.Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+            await Context.SaveChangesAsync();
+        }
+    }
+
+}
