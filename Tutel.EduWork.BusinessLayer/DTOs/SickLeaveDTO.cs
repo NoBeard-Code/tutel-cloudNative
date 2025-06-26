80391eea0175281be42dcfc3233d129b2314c825
Marin Grabovac <grabovacmarin000@gmail.com>
2025-06-26 15:47:57 +0200

    #20 Implemented role based managemnet of the time off

--- Tutel.EduWork.BusinessLayer/DTOs/SickLeaveDTO.cs
+++ Tutel.EduWork.BusinessLayer/DTOs/SickLeaveDTO.cs
@@ -1,11 +1,14 @@
-﻿namespace Tutel.EduWork.BusinessLayer.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutel.EduWork.BusinessLayer.DTOs
 {
     public class SickLeaveDTO
     {
         public int Id { get; set; }
         public required DateOnly StartDate { get; set; }
         public required DateOnly EndDate { get; set; }
-        public string? Reason { get; set; }
+        [Required(ErrorMessage = "Razlog bolovanja je obavezan.")]
+        public string Reason { get; set; }
         public string UserId { get; set; }
         public string? FullName { get; set; }
     }
