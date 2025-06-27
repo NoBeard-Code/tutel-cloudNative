e539518da8762997762135846b37540e8e342a8d
tkolar20 <tkolar20@foi.hr>
2025-06-27 11:56:39 +0200

    #4 Added link to work slice creation

--- Tutel.EduWork.BusinessLayer/DTOs/WorkDayDTO.cs
+++ Tutel.EduWork.BusinessLayer/DTOs/WorkDayDTO.cs
@@ -2,6 +2,7 @@
 {
     public class WorkDayDTO
     {
+        public int Id { get; set; }
         public DateOnly WorkDate { get; set; }
         public TimeOnly WorkDayStart { get; set; }
         public string UserId { get; set; }
