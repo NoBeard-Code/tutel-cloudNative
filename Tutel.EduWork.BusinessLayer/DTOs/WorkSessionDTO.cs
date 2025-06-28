ab3b3c808ba65bae9a45dde89b356944257982f7
Marin Grabovac <grabovacmarin000@gmail.com>
2025-06-28 18:56:19 +0200

    #4 Implemented editing worksession

--- Tutel.EduWork.BusinessLayer/DTOs/WorkSessionDTO.cs
+++ Tutel.EduWork.BusinessLayer/DTOs/WorkSessionDTO.cs
@@ -2,6 +2,7 @@
 {
     public class WorkSessionDTO
     {
+        public int Id { get; set; }
         public TimeOnly StartTime { get; set; }
         public TimeOnly EndTime { get; set; }
         public bool IsOvertime { get; set; }
