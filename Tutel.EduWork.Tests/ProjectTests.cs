404bfdab022f61427dea3f55b14fceed4ecf80c9
andrejatrivic <andreja.trivic25@gmail.com>
2025-06-30 14:38:53 +0200

    #34 Added Project tests

---
+++ Tutel.EduWork.Tests/ProjectTests.cs
@@ -0,0 +1,104 @@
+﻿using Bunit;
+using Bunit.TestDoubles;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Tutel.EduWork.BusinessLayer.Abstractions;
+using Tutel.EduWork.BusinessLayer.DTOs;
+using Tutel.EduWork.Client.Components.Project;
+
+namespace Tutel.EduWork.Tests
+{
+    public class ProjectTests : TestContext
+    {
+        /// <summary>
+        /// Checks if projects are loaded and displayed in table.
+        /// </summary>
+        [Fact]
+        public void Project_ProjectsLoaded()
+        {
+            var mockProjectService = new Mock<IProjectService>();
+
+            var mockProjects = new List<ProjectDTO> {
+                new ProjectDTO { Id = 1, Name = "Projekt A", Description = "Nije projekt A", IsActive = true, IsBillable = true},
+                new ProjectDTO { Id = 2, Name = "Projekt B", Description = "Nije projekt B", IsActive = true, IsBillable = false}
+            };
+
+            mockProjectService.Setup(s => s.GetAllAsync()).ReturnsAsync(mockProjects);
+
+            Services.AddSingleton(mockProjectService.Object);
+
+            var cut = RenderComponent<ProjectList>(); 
+            cut.WaitForState(() => cut.FindAll("tbody tr").Count == mockProjects.Count);
+
+            var rows = cut.FindAll("tbody tr");
+            Assert.Equal(mockProjects.Count, rows.Count);
+
+            Assert.Contains("Projekt A", rows[0].InnerHtml);
+            Assert.Contains("Projekt B", rows[1].InnerHtml);
+        }
+
+        /// <summary>
+        /// Check if on dropdown select displayed projects are changed.
+        /// </summary>
+        [Fact]
+        public void Project_OnSelectChangeProjects()
+        {
+            var mockProjectService = new Mock<IProjectService>();
+
+            var mockProjects = new List<ProjectDTO> {
+                new ProjectDTO { Id = 1, Name = "Projekt A", Description = "Nije projekt A", IsActive = true, IsBillable = true},
+                new ProjectDTO { Id = 2, Name = "Projekt B", Description = "Nije projekt B", IsActive = false, IsBillable = false}
+            };
+            mockProjectService.Setup(s => s.GetAllAsync()).ReturnsAsync(mockProjects);
+
+            Services.AddSingleton(mockProjectService.Object);
+
+            var cut = RenderComponent<ProjectList>(); 
+            cut.WaitForState(() => cut.FindAll("tbody tr").Count == 1);
+
+            var rows = cut.FindAll("tbody tr");
+            Assert.Single(rows);
+            Assert.Contains("Projekt A", rows[0].InnerHtml);
+
+            var select = cut.Find("select");
+
+            select.Change("inactive");
+            cut.WaitForState(() => cut.FindAll("tbody tr").Count == 1);
+            rows = cut.FindAll("tbody tr");
+            Assert.Single(rows);
+            Assert.Contains("Projekt B", rows[0].InnerHtml);
+
+            select.Change("all");
+            cut.WaitForState(() => cut.FindAll("tbody tr").Count == 2);
+            rows = cut.FindAll("tbody tr");
+            Assert.Contains("Projekt A", rows[0].InnerHtml);
+            Assert.Contains("Projekt B", rows[1].InnerHtml);
+        }
+
+        /// <summary>
+        /// Checks if "Uredi" button redirects to edit page.
+        /// </summary>
+        [Fact]
+        public void Project_OnEditRedirectToEditPage()
+        {
+            var mockProjectService = new Mock<IProjectService>();
+            var mockProjects = new List<ProjectDTO> {
+                new ProjectDTO { Id = 1, Name = "Projekt A", Description = "Nije projekt A", IsActive = true, IsBillable = true},
+            };
+            mockProjectService.Setup(s => s.GetAllAsync()).ReturnsAsync(mockProjects);
+
+            Services.AddSingleton(mockProjectService.Object);
+
+            var navigationManager = Services.GetRequiredService<FakeNavigationManager>();
+
+            var cut = RenderComponent<ProjectList>(); 
+            cut.WaitForState(() => cut.FindAll("tbody tr").Count == 1);
+
+            var editButton = cut.Find("button.btn-primary");
+
+            editButton.Click();
+
+            Assert.Equal($"editproject/{mockProjects[0].Id}", navigationManager.Uri.Replace(navigationManager.BaseUri, ""));
+        }
+    }
+}
